60215bf2901b623bdb3487953413206348fdfb88
adipol1359 <aditv1337@gmail.com>
2023-06-13 21:59:16 +0200

    fix: fix parameter type not found

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,9 +1,8 @@
 import { serialize } from 'next-mdx-remote/serialize';
 import rehypeCodeTitles from 'rehype-code-titles';
 import rehypePrism from 'rehype-prism-plus';
-import type { VFileCompatible } from 'vfile';
 
-export const serializeSource = (source: VFileCompatible) =>
+export const serializeSource = (source: string) =>
 	serialize(source, {
 		parseFrontmatter: true,
 		mdxOptions: {
