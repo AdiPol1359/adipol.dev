3497a431ab9d4464b59076e87d28a5cead6fe65a
adipol1359 <aditv1337@gmail.com>
2023-07-19 15:00:12 +0200

    feat: add new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,9 @@
 import DevFAQThumbnail from '@/assets/images/devfaq.png';
 import HappyShopThumbnail from '@/assets/images/happy-shop.png';
+import MobiusThumbnail from '@/assets/images/mobius.png';
 import NodeJSMinecraftItemShop from '@/assets/images/nodejs-minecraft-itemshop.png';
 import NoodlyThumbnail from '@/assets/images/noodly.png';
+import PicstagramThumbnail from '@/assets/images/picstagram.png';
 
 import type { Project } from '@/types';
 
@@ -14,13 +16,62 @@ export const projects: Project[] = [
 		url: 'https://app.devfaq.pl/',
 		technologies: [
 			'TypeScript',
+			'React.js',
 			'Next.js',
-			'Tailwind',
-			'Fastify',
-			'Prisma',
+			'Tailwind CSS',
+			'Vitest',
+			'Tanstack Query',
+			'Storybook',
 			'OpenAPI',
+			'Swagger UI',
+			'Fastify',
+			'Docker',
+			'Prisma',
+			'PostgreSQL',
+		],
+	},
+	{
+		thumbnail: PicstagramThumbnail,
+		title: 'Picstagram',
+		description:
+			'Picstagram to strona internetowa służąca do dzielenia się najlepszymi momentami ze swojego życia poprzez przesyłanie zdjęć. Pozwala na utworzenie prywatnego profilu, na którym możesz następnie zdobywać lajki oraz obserwacje.',
+		url: 'https://picstagram.adipol.dev/',
+		technologies: [
+			'TypeScript',
+			'React.js',
+			'Next.js',
+			'Tailwind CSS',
+			'Vitest',
+			'Rest Testing Library',
+			'Tanstack Query',
+			'React Hook Form',
+			'Storybook',
+			'Docker',
+			'NextAuth',
+			'PostgreSQL',
+			'tRPC',
+		],
+	},
+	{
+		thumbnail: MobiusThumbnail,
+		title: 'Mobius',
+		description:
+			'Aplikacja webowa służąca do zarządzania zespołem w Twojej pracy. Umożliwia utworzenie prywatnej grupy, na którą możesz zaprosić następnie członków całego zespołu. Utworzenie grupy pozwala na szybką i efektywną komunikację wraz z całym zespołem.',
+		url: 'https://mobius.adipol.dev/',
+		technologies: [
+			'TypeScript',
+			'React.js',
+			'Next.js',
+			'React Hook Form',
+			'Storybook',
+			'Tailwind CSS',
+			'Tanstack Query',
 			'Docker',
 			'PostgreSQL',
+			'OpenAPI',
+			'Swagger UI',
+			'Socket.io',
+			'Redis',
 		],
 	},
 	{
@@ -28,12 +79,13 @@ export const projects: Project[] = [
 		title: 'Noodly',
 		description:
 			'Strona internetowa w postaci platformy blogowej, umożliwiająca prowadzenie własnego profilu pisząc artykuły na różne tematy.',
-		url: 'https://noodly.projectcode.pl/',
+		url: 'https://noodly.adipol.dev/',
 		technologies: [
 			'TypeScript',
+			'React.js',
 			'Next.js',
-			'MUI',
 			'React Hook Form',
+			'MUI',
 			'Fastify',
 			'Prisma',
 		],
