691c691b3e0031c958d6ede5e074d790e9325db8
adipol1359 <aditv1337@gmail.com>
2023-07-19 13:06:08 +0200

    refactor: remove mdx components

--- src/components/SinglePost/PostContent.tsx
+++ src/components/SinglePost/PostContent.tsx
@@ -1,19 +1,15 @@
 'use client';
 
-import Image from 'next/image';
-import Link from 'next/link';
 import { MDXRemote } from 'next-mdx-remote';
 
 import type { MDXRemoteSerializeResult } from 'next-mdx-remote';
 
 import '@/styles/prism-theme.css';
 
-const components = { Image, Link };
-
 type PostContentProps = Readonly<{
 	source: MDXRemoteSerializeResult;
 }>;
 
 export const PostContent = ({ source }: PostContentProps) => (
-	<MDXRemote {...source} components={components} />
+	<MDXRemote {...source} />
 );
