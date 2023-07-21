543da137f2c7a3dc1e08fe3ef510c1e46b7f897b
adipol1359 <aditv1337@gmail.com>
2023-07-21 10:14:35 +0200

    feat: add target _blank to project link

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -6,7 +6,12 @@ export const ProjectList = () => (
 	<ul className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2">
 		{projects.map((project) => (
 			<li key={project.title}>
-				<a href={project.url} className="[&>*>img]:hover:scale-[1.02]">
+				<a
+					href={project.url}
+					target="_blank"
+					rel="noreferrer noopener"
+					className="[&>*>img]:hover:scale-[1.02]"
+				>
 					<SingleProject project={project} />
 				</a>
 			</li>
