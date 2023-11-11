1f08a8e8d1e1722219850c668bca6a2e2e11d760
adipol1359 <aditv1337@gmail.com>
2023-11-11 21:19:03 +0100

    chore: update deps

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
-export const LATEST_POSTS_LIMIT = 3;
-
 export const BASE_OPEN_GRAPH = {
 	siteName: 'AdiPol Dev',
 	locale: 'pl_PL',
 	url: 'https://adipol.dev/',
 };
+
+export const THEME_COLOR = '#2563eb';
+
+export const LATEST_POSTS_LIMIT = 3;
