1bdc89d17b00783f913a633fec4c3e9f95e2622b
Adrian Polak <aditv1337@gmail.com>
2024-03-24 20:40:55 +0100

    feat: add color palette (#18)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,23 @@ export default {
 	content: ['./src/**/*.{ts,tsx}', '.storybook/preview.tsx'],
 	theme: {
 		extend: {
+			colors: {
+				danger: '#e14343',
+				primary: '#4f46e5',
+				secondary: {
+					50: '#f5f7fA',
+					100: '#e9eef5',
+					200: '#cedce9',
+					300: '#a3bed6',
+					400: '#729cbe',
+					500: '#5080a7',
+					600: '#3e668b',
+					700: '#335271',
+					800: '#2d475f',
+					900: '#16212c',
+					950: '#0e141b',
+				},
+			},
 			fontFamily: {
 				sans: ['var(--font-poppins)', ...defaultTheme.fontFamily.sans],
 			},
