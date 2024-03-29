c8eb506b070e75c10e2004d8c9149e6d3e37d028
adipol1359 <aditv1337@gmail.com>
2024-03-29 14:44:08 +0100

    refactor: simplify return

--- src/components/layout/Header/HeaderItems/HeaderItemsNavigation/HeaderItemsNavigation.tsx
+++ src/components/layout/Header/HeaderItems/HeaderItemsNavigation/HeaderItemsNavigation.tsx
@@ -2,22 +2,20 @@ import { items } from './HeaderItemsNavigation.items';
 
 import { ActiveLink } from '@/components/common/ActiveLink';
 
-export const HeaderItemsNavigation = () => {
-	return (
-		<nav>
-			<ul className="flex flex-col items-center gap-y-7 desktop:flex-row desktop:gap-x-8">
-				{items.map(({ title, href }) => (
-					<li key={title}>
-						<ActiveLink
-							href={href}
-							activeClassName="text-heading"
-							className="text-nowrap text-light transition-colors hover:text-heading"
-						>
-							{title}
-						</ActiveLink>
-					</li>
-				))}
-			</ul>
-		</nav>
-	);
-};
+export const HeaderItemsNavigation = () => (
+	<nav>
+		<ul className="flex flex-col items-center gap-y-7 desktop:flex-row desktop:gap-x-8">
+			{items.map(({ title, href }) => (
+				<li key={title}>
+					<ActiveLink
+						href={href}
+						activeClassName="text-heading"
+						className="text-nowrap text-light transition-colors hover:text-heading"
+					>
+						{title}
+					</ActiveLink>
+				</li>
+			))}
+		</ul>
+	</nav>
+);
