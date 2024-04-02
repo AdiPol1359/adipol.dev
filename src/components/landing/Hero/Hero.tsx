b0cd2c44cd3126cce3819c3ae7746cb38b1146fe
adipol1359 <aditv1337@gmail.com>
2024-04-02 18:43:59 +0200

    feat: add top margin at mobile phones

--- src/components/landing/Hero/Hero.tsx
+++ src/components/landing/Hero/Hero.tsx
@@ -8,7 +8,7 @@ import { Container } from '@/components/ui/Container/Container';
 export const Hero = () => (
 	<Container
 		as="section"
-		className="flex flex-col items-center gap-y-9 laptop:-mt-40 laptop:h-screen laptop:min-h-hero laptop:flex-row"
+		className="mt-8 flex flex-col items-center gap-y-9 tablet:mt-0 laptop:-mt-40 laptop:h-screen laptop:min-h-hero laptop:flex-row"
 	>
 		<HeroMobileMemoji />
 		<HeroIntroduction />
