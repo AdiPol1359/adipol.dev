14d78b636a4663408d113a41841f1bb9047ef89a
adipol1359 <aditv1337@gmail.com>
2024-04-02 18:58:55 +0200

    refactor(hero): move text color to arrows

--- src/components/landing/Hero/HeroMobileMemoji.tsx
+++ src/components/landing/Hero/HeroMobileMemoji.tsx
@@ -4,10 +4,10 @@ import { twJoin } from 'tailwind-merge';
 import memoji from '@/assets/images/smile-memoji.png';
 import DashedArrow from '@/assets/svg/dashed-arrow-3.svg';
 
-const BASE_ARROW_STYLES = 'absolute top-1/2 translate-y-8';
+const BASE_ARROW_STYLES = 'absolute top-1/2 translate-y-8 text-navy-300';
 
 export const HeroMobileMemoji = () => (
-	<div className="relative text-navy-300 laptop:hidden">
+	<div className="relative laptop:hidden">
 		<Image src={memoji} alt="Smile" width={128} height={154} />
 		<DashedArrow
 			className={twJoin(BASE_ARROW_STYLES, '-left-24 -scale-x-100')}
