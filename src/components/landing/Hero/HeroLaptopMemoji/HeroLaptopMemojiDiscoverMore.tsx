c6e70607273e9680e35b9f95abf558bbae09f1b3
Adrian Polak <aditv1337@gmail.com>
2024-04-07 18:56:10 +0200

    feat(hero): improve discover more anchor (#27)

--- src/components/landing/Hero/HeroLaptopMemoji/HeroLaptopMemojiDiscoverMore.tsx
+++ src/components/landing/Hero/HeroLaptopMemoji/HeroLaptopMemojiDiscoverMore.tsx
@@ -2,12 +2,16 @@ import ArrowDown from '@/assets/svg/arrow-down.svg';
 import { Emoji } from '@/components/common/Emoji';
 
 export const HeroLaptopMemojiDiscoverMore = () => (
-	<div className="absolute -bottom-44 left-0 w-full space-y-7 text-center text-heading">
+	<div className="absolute -bottom-44 left-0 flex w-full flex-col items-center gap-y-7 text-heading">
 		<p className="text-lg" aria-hidden>
 			<Emoji type="💎" /> Odkryj więcej na dole <Emoji type="💎" />
 		</p>
-		<button type="button" aria-label="Odkryj więcej na dole">
+		<a
+			href="#recent-posts"
+			aria-label="Odkryj więcej na dole"
+			className="animate-bounce"
+		>
 			<ArrowDown aria-hidden />
-		</button>
+		</a>
 	</div>
 );
