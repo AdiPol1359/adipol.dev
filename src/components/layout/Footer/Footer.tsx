d7cf98a65afccc5a2fa4a751fc11a773205881d4
adipol1359 <aditv1337@gmail.com>
2024-04-07 14:47:22 +0200

    refactor(layout): refactor footer wrapper styles

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { twMerge } from 'tailwind-merge';
+import { twJoin } from 'tailwind-merge';
 
 import { FooterContainerDecoration } from './FooterContainerDecoration';
 import { FooterCopyright } from './FooterCopyright';
@@ -10,18 +10,23 @@ import { FooterVercelStatus } from './FooterVercelStatus';
 import { Container } from '@/components/ui/Container/Container';
 
 const WRAPPER_STYLES =
-	'flex flex-col items-center text-center laptop:items-start laptop:text-start';
+	'flex flex-col items-center text-center laptop:text-start';
 
 export const Footer = () => (
 	<footer className="relative mt-36 bg-secondary/30 before:absolute before:left-0 before:top-0 before:h-1 before:w-full before:bg-border before:bg-center before:bg-repeat-x">
 		<FooterMemoji />
 		<Container className="relative space-y-10 pb-12 pt-24 laptop:flex laptop:justify-between laptop:space-y-0 laptop:py-14">
-			<div className={twMerge(WRAPPER_STYLES, 'gap-y-10 laptop:gap-y-8')}>
+			<div
+				className={twJoin(
+					WRAPPER_STYLES,
+					'gap-y-10 laptop:items-start laptop:gap-y-8',
+				)}
+			>
 				<FooterSocials />
 				<FooterCopyright />
 			</div>
 			<div
-				className={twMerge(
+				className={twJoin(
 					WRAPPER_STYLES,
 					'gap-y-3.5 laptop:items-end laptop:gap-y-5',
 				)}
