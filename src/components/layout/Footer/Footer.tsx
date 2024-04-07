683b58ad00e7563f6ae98bfea95c29d75102a4d6
adipol1359 <aditv1337@gmail.com>
2024-04-07 14:43:40 +0200

    refactor: merge wrapper styles

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { twJoin } from 'tailwind-merge';
+import { twMerge } from 'tailwind-merge';
 
 import { FooterContainerDecoration } from './FooterContainerDecoration';
 import { FooterCopyright } from './FooterCopyright';
@@ -16,12 +16,12 @@ export const Footer = () => (
 	<footer className="relative mt-36 bg-secondary/30 before:absolute before:left-0 before:top-0 before:h-1 before:w-full before:bg-border before:bg-center before:bg-repeat-x">
 		<FooterMemoji />
 		<Container className="relative space-y-10 pb-12 pt-24 laptop:flex laptop:justify-between laptop:space-y-0 laptop:py-14">
-			<div className={twJoin(WRAPPER_STYLES, 'gap-y-10 laptop:gap-y-8')}>
+			<div className={twMerge(WRAPPER_STYLES, 'gap-y-10 laptop:gap-y-8')}>
 				<FooterSocials />
 				<FooterCopyright />
 			</div>
 			<div
-				className={twJoin(
+				className={twMerge(
 					WRAPPER_STYLES,
 					'gap-y-3.5 laptop:items-end laptop:gap-y-5',
 				)}
