ddf49fd70ce044d44fdf8a1d00740cec94f05b26
Adrian Polak <aditv1337@gmail.com>
2024-04-14 19:11:00 +0200

    feat(storybook): add dark background (#30)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import resolveConfig from 'tailwindcss/resolveConfig';
 
 import { poppins } from '../src/lib/fonts';
+import tailwindConfig from '../tailwind.config';
 
 import type { Preview } from '@storybook/react';
 
 import '../src/assets/styles/tailwind.css';
 
+const fullConfig = resolveConfig(tailwindConfig);
+
 const preview: Preview = {
 	parameters: {
+		backgrounds: {
+			default: 'dark',
+			values: [
+				{
+					name: 'dark',
+					value: fullConfig.theme.colors.navy[950],
+				},
+			],
+		},
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
