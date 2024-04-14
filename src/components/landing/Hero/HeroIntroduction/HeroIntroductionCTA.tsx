bbd64e8400404e0b03a58430ca0b58a54e5a2b54
adipol1359 <aditv1337@gmail.com>
2024-04-14 16:07:40 +0200

    refactor(hero): change buttons to links

--- src/components/landing/Hero/HeroIntroduction/HeroIntroductionCTA.tsx
+++ src/components/landing/Hero/HeroIntroduction/HeroIntroductionCTA.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+
 import { Button } from '@/components/ui/Button/Button';
 
 export const HeroIntroductionCTA = () => (
 	<div className="flex gap-x-3.5 laptop:gap-x-4">
-		<Button variant="outline">Moje portfolio</Button>
-		<Button color="navy.700" variant="outline">
-			Blog
+		<Button variant="outline" asChild>
+			<Link href="/projekty">Moje portfolio</Link>
+		</Button>
+		<Button color="navy.700" variant="outline" asChild>
+			<Link href="#">Blog</Link>
 		</Button>
 	</div>
 );
