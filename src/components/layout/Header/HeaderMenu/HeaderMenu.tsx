3bcff9fd3965b9824cc63c023e6028ab19227d77
adipol1359 <aditv1337@gmail.com>
2024-04-14 16:20:39 +0200

    refactor(hero): change items gap

--- src/components/layout/Header/HeaderMenu/HeaderMenu.tsx
+++ src/components/layout/Header/HeaderMenu/HeaderMenu.tsx
@@ -3,7 +3,7 @@ import { HeaderMenuNavigation } from './HeaderMenuNavigation/HeaderMenuNavigatio
 import { Button } from '@/components/ui/Button/Button';
 
 export const HeaderMenu = () => (
-	<div className="flex flex-col gap-y-6 rounded-xl border-2 border-primary/40 bg-secondary p-5 tablet:flex tablet:flex-row tablet:items-center tablet:gap-x-10 tablet:rounded-none tablet:border-none tablet:bg-transparent tablet:p-0">
+	<div className="flex flex-col gap-y-6 rounded-xl border-2 border-primary/40 bg-secondary p-5 tablet:flex tablet:flex-row tablet:items-center tablet:gap-x-8 tablet:rounded-none tablet:border-none tablet:bg-transparent tablet:p-0">
 		<HeaderMenuNavigation />
 		<Button variant="outline" fill>
 			Kontakt
