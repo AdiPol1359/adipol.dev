2cc47f7d31c3238d57a630f11c81c0a2a241d008
Adrian Polak <aditv1337@gmail.com>
2025-06-30 22:54:42 +0200

    feat(main-layout): add decorations (#38)

--- src/components/decorations/index.ts
+++ src/components/decorations/index.ts
@@ -1,2 +1,4 @@
 export { default as Arrow } from './arrow.astro';
+export { default as Lines } from './lines.astro';
 export { default as Nickname } from './nickname.astro';
+export { default as Waves } from './waves.astro';
