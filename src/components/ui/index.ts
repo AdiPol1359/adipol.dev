f897f6fa6fa94ec23840f5f417816cb6b3ee4ea3
AdiPol1359 <aditv1337@gmail.com>
2025-06-30 20:55:04 +0200

    feat(home): add hero section

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,7 @@
 export { default as Avatar } from './avatar.astro';
 export { default as Button } from './button.astro';
 export { default as Container } from './container.astro';
+export { default as Emoji } from './emoji.astro';
+export { default as Heading } from './heading.astro';
 export { default as Link } from './link.astro';
 export { default as Text } from './text.astro';
