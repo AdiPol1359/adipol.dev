23cd86939eb81045bc35bc2dbaf8ecf2fb08e9fb
Adrian Polak <aditv1337@gmail.com>
2025-07-01 20:51:41 +0200

    refactor(ui): remove typography components (#40)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,3 @@ export { default as Button } from './button.astro';
 export { default as Container } from './container.astro';
 export { default as Emoji } from './emoji.astro';
 export { default as Heading } from './heading.astro';
-export { default as Link } from './link.astro';
-export { default as Text } from './text.astro';
