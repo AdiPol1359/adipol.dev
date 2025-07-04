ebd6b76121d5572489548f8d6d950fb661dd57d6
AdiPol1359 <aditv1337@gmail.com>
2025-07-04 19:04:32 +0200

    feat: add more decorations

--- src/components/decorations/index.ts
+++ src/components/decorations/index.ts
@@ -1,4 +1,7 @@
 export { default as Arrow } from './arrow.astro';
 export { default as Lines } from './lines.astro';
 export { default as Nickname } from './nickname.astro';
+export { default as Scribble } from './scribble.astro';
+export { default as Wave } from './wave.astro';
 export { default as Waves } from './waves.astro';
+export { default as WavyLine } from './wavy-line.astro';
