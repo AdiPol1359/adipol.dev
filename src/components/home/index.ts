751020fc0049dac38d75007d22d82e70f75b4d66
Adrian Polak <aditv1337@gmail.com>
2025-07-04 19:08:09 +0200

    feat: add more decorations (#43)

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,2 +1,3 @@
+export { default as Decorations } from './decorations.astro';
 export { default as HeroSection } from './hero-section/hero-section.astro';
 export { default as LatestPostsSection } from './latest-posts-section/latest-posts-section.astro';
