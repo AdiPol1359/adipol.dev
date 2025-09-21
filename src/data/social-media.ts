542fcdec95d72b55a3eef3b12aa05bc43ba933e7
Adrian Polak <aditv1337@gmail.com>
2025-09-21 17:37:58 +0200

    refactor(blocks): refactor social media (#50)

---
+++ src/data/social-media.ts
@@ -0,0 +1,17 @@
+import { Github, Instagram, Linkedin } from '@/components/icons';
+
+import type { SocialMedia } from '@/types/social-media';
+
+export const socialMedia: SocialMedia[] = [
+	{ icon: Github, name: 'GitHub', url: 'https://github.com/adipol1359' },
+	{
+		icon: Linkedin,
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/polak-adrian',
+	},
+	{
+		icon: Instagram,
+		name: 'Instagram',
+		url: 'https://www.instagram.com/adipol_dev',
+	},
+];
