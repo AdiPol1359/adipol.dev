e67d5b66f9ed5a598bbbaeb8ef73dd316792db92
Adrian Polak <aditv1337@gmail.com>
2025-09-29 19:18:50 +0200

    feat: optimize images (#58)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	image: { layout: 'constrained' },
 	markdown: {
 		shikiConfig: { theme: 'nord' },
 	},
