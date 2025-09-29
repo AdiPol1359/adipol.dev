cf8ac12f62e954629aedf220c3039a179fa96ae4
Adrian Polak <aditv1337@gmail.com>
2025-09-29 20:50:02 +0200

    fix: fix mobile animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	prefetch: {
+		prefetchAll: true,
+	},
 	markdown: {
 		shikiConfig: { theme: 'nord' },
 	},
