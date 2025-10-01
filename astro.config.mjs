158a06c7448766d2e14bbfc65b58db4086abfa3f
Adrian Polak <aditv1337@gmail.com>
2025-10-01 21:19:09 +0200

    feat(main-layout): add SEO head tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	site: 'https://beta.adipol.dev',
 	markdown: {
 		shikiConfig: { theme: 'nord' },
 	},
---
+++ src/components/layouts/main-layout/main-layout.types.ts
@@ -0,0 +1,17 @@
+interface OpenGraphArticle {
+	readonly type: 'article';
+	readonly publishedTime: Date;
+	readonly author: string;
+	readonly tags: string[];
+}
+
+interface OpenGraphWebsite {
+	readonly type: 'website';
+}
+
+export interface HeadOptions {
+	readonly title?: string;
+	readonly description?: string;
+	readonly image?: ImageMetadata;
+	readonly openGraph?: OpenGraphArticle | OpenGraphWebsite;
+}
