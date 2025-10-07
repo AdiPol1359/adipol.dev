3892e2cb0f277ab706132cf5a6c81d847e3f1549
Adrian Polak <aditv1337@gmail.com>
2025-10-07 18:49:40 +0200

    chore: change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	site: 'https://beta.adipol.dev',
+	site: 'https://adipol.dev',
 	markdown: {
 		shikiConfig: { theme: 'nord' },
 	},
